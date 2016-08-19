29acfe1b5a1c0052c3a0898daa24960580b7569d
adevbungert <antoinebungert7@gmail.com>
2016-08-19 11:59:48 -0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  resources :tasks do
+    member do
+      put :change
+    end
+  end
+
   devise_for :users
   get 'pages/home'
 
   root 'pages#home'
 
-  get 'pages/about'
+  get 'about' => "pages#about"
 
 end
